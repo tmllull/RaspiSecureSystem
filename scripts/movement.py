9ead50a600beff054d08f058dbcdff63d876d72c
Toni Miquel <netyaco@gmail.com>
2016-11-17 17:47:25 +0100

    Create movement.py

---
+++ scripts/movement.py
@@ -0,0 +1,15 @@
+import serial
+import os
+import time
+
+arduino=serial.Serial('/dev/ttyACM0',9600)
+#arduino.open()
+
+while True:
+	time.sleep(0.5)
+	x = arduino.read().rstrip()
+	if x == ("d"):
+		print "Movement detected"
+#		os.system("/home/pi/scripts/notification.sh > /dev/null") 
+	arduino.flushInput()
+arduino.close()
